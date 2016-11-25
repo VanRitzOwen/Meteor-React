79c9574e23e8e3a5c36144ae91612ede18a5c597
VanRitzOwen <zbwen406@yeah.net>
2016-11-25 16:23:34 +0800

    Temporary UI state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -17,6 +17,14 @@ class App extends Component {
     //         { _id: 3, text: 'This is task 3' },
     //     ];
     // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
     handleSubmit(event) {
         event.preventDefault();
 
@@ -32,8 +40,18 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task}/>
         ));
     }
@@ -42,7 +60,18 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
+
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                         <input
                             type="text"
@@ -66,6 +95,7 @@ class App extends Component {
     export default createContainer(() => {
         return{
             tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+            incompleteCount: PropTypes.number.isRequired,
     };
 },App);
 
